24ba32d60537db470b2c53f92784419f73724de3
Ikhtiyor <ihtiyor2655742@gmail.com>
2024-04-03 07:28:51 +0500

    filter deleted

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -2,7 +2,6 @@ import { Container } from "@chakra-ui/react";
 import React from "react";
 import Showcase from "./Showcase";
 import Categories from "./Categories";
-// import Filter from "../../search/Filter";
 import Cards from "../../partials/Cards";
 
 function Home() {
@@ -16,8 +15,7 @@ function Home() {
           lg: "900px",
           xl: "1200px",
         }}
-        // maxW={{ base: "1200px" }}
-        // maxW="1200px"
+      
         mx="auto"
         my="0"
       >
@@ -26,7 +24,6 @@ function Home() {
       </Container>
       <hr />
       <Container maxW="1200px" mx="auto" my="0">
-        {/* <Filter /> */}
         <Cards />
       </Container>
     </>
